dffa19050dadbb2cc90a15c931e5ae575ddb9b74
raviCodez <ravivarmanb025@gmail.com>
2024-10-19 21:54:24 +0530

    Add files via upload

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.getSecondLargest(arr);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int len=arr.length;
+        if (len<2){
+            return Integer.MIN_VALUE;
+        }
+        int largest =arr[0];
+        int slargest=Integer.MIN_VALUE;
+        for (int i=1;i<len;i++){
+            if(arr[i]>largest){
+                slargest=largest;
+                largest =arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>slargest){
+                slargest=arr[i];
+            }
+            
+        }
+        return slargest;
+        
+        
+        
+        // Code Here
+    }
+}
